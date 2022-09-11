2032de2275724968b6d1210ef801df94babbdd4f
samar <samarkhalel95@gmail.com>
2022-09-11 17:39:01 +0300

    working on ChooseLevelGroup and ChoosePicGroup folders and editing the files without react effect

--- components/ChooseLevelGroup/ChooseLevelGroup.jsx
+++ components/ChooseLevelGroup/ChooseLevelGroup.jsx
@@ -1,15 +1,39 @@
 import React from "react";
-import Group5 from "./Group5";
+
+const levels = [
+  "easy",
+  "middle",
+  "hard"
+];
 
 export default function ChooseLevelGroup(props) {
+  const easy = props.easy;
+  const setEasy = props.setEasy;
+  const easyClick = () => setEasy(!easy);
+  const middle = props.setMiddle;
+  const setMiddle = props.setMiddle;
+  const middleClick = () => setMidddle(!middle);
+  const hard = props.hard;
+  const setHard = props.setHard;
+  const hardClick = () => setHard(!hard);
   return (
-    <label htmlFor="idn2">
-      <label className="group4">
-        <h3>Choose level</h3>
-      </label>
-      <form id="levelClick">
-        <Group5 props={props} />
-      </form>
-    </label>
+    <fieldset>
+      <legend>Choose level</legend>
+      {levels.map(lev => (
+        <label className="levels">
+         <div> onClick={() => {easyClick();}} </div>
+        </label>
+      ))}
+     {levels.map(lev => (
+        <label className="levels">
+        <div> onClick={() => {middleClick();}} </div>
+        </label>
+      ))}
+     {levels.map(lev => (
+        <label className="levels">
+        <div> onClick={() => {hardClick();}} </div>
+        </label>
+      ))}
+    </fieldset>
   );
 }
--- components/ChoosePicGroup/ChoosePicGroup.jsx
+++ components/ChoosePicGroup/ChoosePicGroup.jsx
@@ -1,15 +1,42 @@
 import React from "react";
-import Group3 from "../ChoosePicGroup/Group3";
+
+var imgsArray = new Array();
+imgsArray[0] = new Image();
+imgsArray[0].src = './public/pic/pic1.jpg';
+imgsArray[1] = new Image();
+imgsArray[1].src = './public/pic/pic2.jpg';
+imgsArray[2] = new Image();
+imgsArray[2].src = './public/pic/pic3.jpg';
+
 
 export default function ChoosePicGroup(props) {
+  const pic1 = props.pic1;
+  const setPic1 = props.setPic1;
+  const pic1Click = () => setPic1(!pic1);
+  const pic2 = props.pic2;
+  const setPic2 = props.setPic2;
+  const pic2Click = () => setPic2(!pic2);
+  const pic3 = props.pic3;
+  const setPic3 = props.setPic3;
+  const pic3Click = () => setPic3(!pic3);
   return (
-    <label htmlFor="idn2">
-      <label className="group2">
-        <h3>Choose picture</h3>
-      </label>
-      <form id="picClick">
-        <Group3 props={props} />
-      </form>
-    </label>
+    <fieldset>
+      <legend>Choose picture</legend>
+      {imgsArray.map(pic => (
+        <label className="pictures">
+        <img src="/pic/pic1.jpg" onClick={() => {pic1Click();}}/>
+        </label>
+      ))}
+     {imgsArray.map(pic => (
+        <label className="pictures">
+        <img src="/pic/pic2.jpg" onClick={() => {pic2Click();}}/>
+        </label>
+      ))}
+     {imgsArray.map(pic => (
+        <label className="pictures">
+        <img src="/pic/pic3.jpg" onClick={() => {pic3Click();}}/>
+        </label>
+      ))}
+    </fieldset>
   );
 }
