d8bd8f5a97c792cf334904599d7a099bf880ac05
Asmaalafe55 <asmaalafe55@gmail.com>
2022-09-11 18:41:45 +0300

    bla

--- components/ChooseLevelGroup/ChooseLevelGroup.jsx
+++ components/ChooseLevelGroup/ChooseLevelGroup.jsx
@@ -1,39 +1,47 @@
 import React from "react";
 
-const levels = [
-  "easy",
-  "middle",
-  "hard"
-];
-
 export default function ChooseLevelGroup(props) {
   const easy = props.easy;
   const setEasy = props.setEasy;
   const easyClick = () => setEasy(!easy);
   const middle = props.setMiddle;
   const setMiddle = props.setMiddle;
-  const middleClick = () => setMidddle(!middle);
+  const middleClick = () => setMiddle(!middle);
   const hard = props.hard;
   const setHard = props.setHard;
   const hardClick = () => setHard(!hard);
   return (
-    <fieldset>
-      <legend>Choose level</legend>
-      {levels.map(lev => (
-        <label className="levels">
-         <div> onClick={() => {easyClick();}} </div>
-        </label>
-      ))}
-     {levels.map(lev => (
-        <label className="levels">
-        <div> onClick={() => {middleClick();}} </div>
-        </label>
-      ))}
-     {levels.map(lev => (
-        <label className="levels">
-        <div> onClick={() => {hardClick();}} </div>
-        </label>
-      ))}
-    </fieldset>
+    <label>
+      <div>Choose level</div>
+      <label className="levels">
+        <div
+          onClick={() => {
+            easyClick();
+          }}
+        >
+          easy
+        </div>
+      </label>
+
+      <label className="levels">
+        <div
+          onClick={() => {
+            middleClick();
+          }}
+        >
+          middle
+        </div>
+      </label>
+
+      <label className="levels">
+        <div
+          onClick={() => {
+            hardClick();
+          }}
+        >
+          hard
+        </div>
+      </label>
+    </label>
   );
 }
--- components/ChoosePicGroup/ChoosePicGroup.jsx
+++ components/ChoosePicGroup/ChoosePicGroup.jsx
@@ -2,12 +2,11 @@ import React from "react";
 
 var imgsArray = new Array();
 imgsArray[0] = new Image();
-imgsArray[0].src = './public/pic/pic1.jpg';
+imgsArray[0].src = "./pic/pic1.jpg";
 imgsArray[1] = new Image();
-imgsArray[1].src = './public/pic/pic2.jpg';
+imgsArray[1].src = "./pic/pic2.jpg";
 imgsArray[2] = new Image();
-imgsArray[2].src = './public/pic/pic3.jpg';
-
+imgsArray[2].src = "./pic/pic3.jpg";
 
 export default function ChoosePicGroup(props) {
   const pic1 = props.pic1;
@@ -20,23 +19,37 @@ export default function ChoosePicGroup(props) {
   const setPic3 = props.setPic3;
   const pic3Click = () => setPic3(!pic3);
   return (
-    <fieldset>
-      <legend>Choose picture</legend>
-      {imgsArray.map(pic => (
-        <label className="pictures">
-        <img src="/pic/pic1.jpg" onClick={() => {pic1Click();}}/>
-        </label>
-      ))}
-     {imgsArray.map(pic => (
-        <label className="pictures">
-        <img src="/pic/pic2.jpg" onClick={() => {pic2Click();}}/>
-        </label>
-      ))}
-     {imgsArray.map(pic => (
-        <label className="pictures">
-        <img src="/pic/pic3.jpg" onClick={() => {pic3Click();}}/>
-        </label>
-      ))}
-    </fieldset>
+    <label>
+      <div>Choose picture</div>
+      <label>
+        <img
+          className="pic1"
+          src="/pic/pic1.jpg"
+          onClick={() => {
+            pic1Click();
+          }}
+        />
+      </label>
+
+      <label>
+        <img
+          className="pic2"
+          src="/pic/pic2.jpg"
+          onClick={() => {
+            pic2Click();
+          }}
+        />
+      </label>
+
+      <label>
+        <img
+          className="pic3"
+          src="/pic/pic3.jpg"
+          onClick={() => {
+            pic3Click();
+          }}
+        />
+      </label>
+    </label>
   );
 }
