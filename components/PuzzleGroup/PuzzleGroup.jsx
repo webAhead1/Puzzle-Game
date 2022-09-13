5ef0fbe89aff73bab0529deae8a3ba9581452a23
samar <samarkhalel95@gmail.com>
2022-09-13 21:54:21 +0300

    adding matrix

---
+++ components/PuzzleGroup/PuzzleGroup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import EasyLevelPuzzle from "./EasyLevelPuzzle";
+
+export default function PuzzleGroup(props){
+    return (
+        <EasyLevelPuzzle/>
+    );
+}
+
