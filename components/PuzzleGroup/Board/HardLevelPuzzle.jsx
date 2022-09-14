73b20c9dc259ebe49a86c650aa3d2e388029a689
samar <samarkhalel95@gmail.com>
2022-09-14 20:05:35 +0300

    samar

--- components/PuzzleGroup/Board/HardLevelPuzzle.jsx
+++ components/PuzzleGroup/Board/HardLevelPuzzle.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Board from "./Board";
+import EmptyBoard from "./EmptyBoard";
 
 export default function HardLevelPuzzle(props) {
   const [level, setLevel] = React.useState(5);
